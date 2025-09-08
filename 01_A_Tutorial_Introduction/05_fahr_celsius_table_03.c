7d67fa0ed30cb6d135ffcdb0d919ea02a7abae65
artc-msc <artc.msc+git@pm.me>
2025-09-08 01:03:11 -0400

    Add fahr-celsius program with heading

---
+++ 01_A_Tutorial_Introduction/05_fahr_celsius_table_03.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300; floating-point
+ * version */
+int main(void)
+{
+	float fahr, celsius;
+	float lower, upper, step;
+
+	lower = 0;		/* lower limit of temperature scale */
+	upper = 300;	/* upper limit */
+	step = 20;		/* step size */
+
+	fahr = lower;
+
+	printf("  F    C\n");
+	printf("----------\n");
+
+	while (fahr <= upper) {
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+	return EXIT_SUCCESS;
+}
