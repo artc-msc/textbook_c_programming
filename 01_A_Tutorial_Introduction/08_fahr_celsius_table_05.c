a13feec8c20348b94f64eb4a5a1c44aa3fccbbe1
artc-msc <artc.msc+git@pm.me>
2025-09-08 01:35:14 -0400

    Add fahr-celsius program that prints in reverse

---
+++ 01_A_Tutorial_Introduction/08_fahr_celsius_table_05.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* print Fahrenheit-Celsius table in reverse */ 
+int main(void)
+{
+	int fahr;
+	
+	for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+	return EXIT_SUCCESS;
+}
