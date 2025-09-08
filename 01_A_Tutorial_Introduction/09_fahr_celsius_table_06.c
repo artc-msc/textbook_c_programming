41a0eeb2f8e738fa3f9caadc36ed291e0b07f6a7
artc-msc <artc.msc+git@pm.me>
2025-09-08 01:51:48 -0400

    Add fahr-celsius program with macros

---
+++ 01_A_Tutorial_Introduction/09_fahr_celsius_table_06.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LOWER_TEMP 0
+#define UPPER_TEMP 300
+#define TEMP_INCREMENT 20
+
+/* print Fahrenheit-Celsius table */ 
+int main(void)
+{
+	int fahr;
+	
+	for (fahr = LOWER_TEMP; fahr <= UPPER_TEMP; fahr = fahr + TEMP_INCREMENT)
+		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+	return EXIT_SUCCESS;
+}
