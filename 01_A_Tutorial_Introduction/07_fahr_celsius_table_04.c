4dd2ce75de7e8514630771015277f63356e5dbb0
artc-msc <artc.msc+git@pm.me>
2025-09-08 01:31:28 -0400

    Add fahr-celsius program using for loop

---
+++ 01_A_Tutorial_Introduction/07_fahr_celsius_table_04.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* print Fahrenheit-Celsius table */ 
+int main(void)
+{
+	int fahr;
+	
+	for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+		printf("%d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+	return EXIT_SUCCESS;
+}
